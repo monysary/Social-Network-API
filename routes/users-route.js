0f44342a99f71437746ab7ec17bf5d35348a1496
monysary <sary.mony@gmail.com>
2023-01-15 19:44:47 -0800

    added route for deleting user

--- routes/users-route.js
+++ routes/users-route.js
@@ -39,12 +39,22 @@ router.post('/', async (req, res) => {
 // Update a user
 router.put('/:id', async (req, res) => {
     try {
-        const updateUser = await User.findByIdAndUpdate(req.params.id, req.body);
+        await User.findByIdAndUpdate(req.params.id, req.body);
         res.status(200).json({ message: 'User updated!' });
     } catch (err) {
         res.status(500).json(err);
         console.log(err);
     }
-})
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'User deleted!' })
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err);
+    }
+});
 
 module.exports = router;
